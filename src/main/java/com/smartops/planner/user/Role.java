31dd4b990607e9392320a73cc47a6fcbd378b747
Borja <borjagonzare@gmail.com>
2026-05-27 14:50:51 +0200

    Añadir modelo de usuarios y roles

---
+++ src/main/java/com/smartops/planner/user/Role.java
@@ -0,0 +1,7 @@
+package com.smartops.planner.user;
+
+public enum Role {
+    ADMIN,
+    MANAGER,
+    EMPLOYEE
+}
---
+++ src/main/java/com/smartops/planner/user/UserRepository.java
@@ -0,0 +1,11 @@
+package com.smartops.planner.user;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsernameIgnoreCase(String username);
+
+    boolean existsByUsernameIgnoreCase(String username);
+}
